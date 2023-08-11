f86e1c9727717bb1daec74bf50e426c592e1c9d0
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2023-08-10 22:05:08 -0700

    early morning

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prograam to print text ussing  prriintf
+ * Return: 0 (successs)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

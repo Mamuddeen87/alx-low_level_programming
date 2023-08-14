391dc8e6dfe6f7ed86d5fbf31a141ddcc77706ac
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2023-08-13 17:24:05 -0700

    lo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program to print possible combinations of two digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+			if (i != j)
+			{
+				printf("%d %d, i, j");
+				if (i != 98 || j != 99)
+					printf(", ");
+			}
+	}
+	putchar('\n');
+	return (0);
+}

88fbcd813463285d5bea4adab2d65c95dff3c1e0
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2023-08-15 04:11:50 -0700

    main

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,20 @@ int main(void)
 {
 	int m, n;
 
-	for (m = '0'; m <= '9'; m++)
-	for (n = '0'; n <= '9'; n++)
-	{
-		if (m != n)
+	for (m = 0; m <= 9; m++)
+		for (n = 0; n <= 9; n++)
 		{
-		putchar(m);
-		putchar(n);
-		putchar(',');
-		putchar(' ');
+			if (m != n)
+			{
+				putchar(m);
+				putchar(n);
+				if (m == 9 && n == 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }

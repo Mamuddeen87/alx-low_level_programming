737c2e036992b0d55b973de572386846a39b67ce
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2023-08-16 04:12:25 -0700

    morning

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 /**
- * main - a program to print _putchar followed  by a new line
+ * main - a program to print _putchar followed by a new line.
  *
- * return: 0 (Success)
- */
-/* 
- * main - program to print given string
- * return: 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,15 +2,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		printf("%d", 1);
 		return (1);
 	}
 	if (n == 0)
 	{
-		putchar(48);
+		putchar("%d", 0);
 		return (0);
 	}
-	putchar('-');
-	putchar('1');
+	printf("%c", );
 	return (-1);
 }

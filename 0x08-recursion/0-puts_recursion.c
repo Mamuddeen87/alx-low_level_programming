f8125753c5b0f63ffa4313ab3eacb9cbae33f8c8
root <bellog.ug@atbu.edu.ng>
2023-08-30 16:12:13 -0700

    1st

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion(char *s) - print a string folllowedd  byy new line
+ * @s - the e  strinng
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+			return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+/**
+ * main - entry point  off thee prrogrramm
+ * Return: 0 alwayys
+ */
+int main(void)
+	{
+		_puts_recursion("Recursion");
+		return (0);
+	}

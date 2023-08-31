41604758386f6389eca350883d1fbd45401301c3
root <bellog.ug@atbu.edu.ng>
2023-08-30 17:15:37 -0700

    Hi

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,17 +2,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_print_rev_recursion(s + 1);
-	putchar(*s);
-}
-
-int main(void)
-{
-	char str[] = "Mamuddeen";
-	_print_rev_recursion(str);
-	return (0);
 }

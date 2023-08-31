13dc44d76efeb601a20f01f9985a3be277e92ff4
root <bellog.ug@atbu.edu.ng>
2023-08-30 17:10:27 -0700

    ok

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,21 +7,13 @@
 
 void _puts_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	if (*s == '\0')
 	{
 		putchar('\n');
-			return;
 	}
-	putchar(*s);
-	_puts_recursion(s + 1);
 }
-
-/**
- * main - entry point  off thee prrogrramm
- * Return: 0 alwayys
- */
-int main(void)
-	{
-		_puts_recursion("The recursion task");
-		return (0);
-	}
